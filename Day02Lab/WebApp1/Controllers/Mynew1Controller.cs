009e2ebfce25c092a39da0b6c5a7d0be5c268e21
Nguyenvanha2509 <nguyenha25092005@gmail.com>
2025-08-28 23:46:39 +0700

    Add files via upload

---
+++ Day02Lab/WebApp1/Controllers/Mynew1Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class Mynew1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sample()
+        {
+            return View();
+        }
+        public IActionResult error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day02Lab/WebApp1/Controllers/Mynew2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class Mynew2Controller : Controller
+    {
+        public IActionResult TrangChu()
+        {
+            return View();
+        }
+        public IActionResult CaiDat()
+        {
+            return View();
+        }
+    }
+}
