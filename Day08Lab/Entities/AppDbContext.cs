06517e37c7b8e9d23c1cae7f472797236e8b3a85
Nguyenvanha2509 <nguyenha25092005@gmail.com>
2025-10-11 23:53:04 +0700

    Them thu muc Day08lab

---
+++ Day08Lab/Entities/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Day08Lab.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Day08Lab.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Day08Lab/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Day08Lab.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Tên danh mục không được để trống")]
+        [StringLength(100)]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "tinyint")]
+        public byte Status { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        // sản phẩm thuộc danh mục
+        public ICollection<Product>? Products { get; set; }
+    }
+}
