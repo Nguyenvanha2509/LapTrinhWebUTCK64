d123a1fcefa63d08acd384e873f9589c07e04140
Nguyenvanha2509 <nguyenha25092005@gmail.com>
2025-11-07 23:53:27 +0700

    Them thu muc thuc hanh day12lab

---
+++ lab1/lab1/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace lab1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }     //ma sv
+        public string? Name { get; set; }   //ho ten
+        public string? Email { get; set; }  //email
+        public String? Password { get; set; }   //mat khau
+        public Branch? Branch { get; set; } //nganh hoc
+        public Gender? Gender { get; set; } // gioi tinh
+        public bool IsRegular { get; set; }  //he:true-chinh quy; false- phi chinh quy
+        public string? Address { get; set; }    //dia chi
+        public DateTime DateOfBorth { get; set; }   //ngay sinh
+
+    }
+}
