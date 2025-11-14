b71f68a91787495aa1cacf36a066c80057029b45
Nguyenvanha2509 <nguyenha25092005@gmail.com>
2025-11-14 23:52:52 +0700

    them bai tap lab3 va lab4

---
+++ lab3_4/lab1/Data/DbInitializer.cs
@@ -0,0 +1,60 @@
+﻿using lab1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab1.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SchoolContext(
+                serviceProvider.GetRequiredService<DbContextOptions<SchoolContext>>()))
+            {
+                context.Database.EnsureCreated();
+
+                // Nếu đã có dữ liệu thì không thêm nữa
+                if (context.Majors.Any()) return;
+
+                // Thêm Major
+                var majors = new Major[]
+                {
+                    new Major{MajorName="IT"},
+                    new Major{MajorName="Economics"},
+                    new Major{MajorName="Mathematics"}
+                };
+                context.Majors.AddRange(majors);
+                context.SaveChanges();
+
+                // Thêm Learner
+                var learners = new Learner[]
+                {
+                    new Learner{FirstMidName="Carson", LastName="Alexander", EnrollmentDate=DateTime.Parse("2005-09-01"), MajorID=1},
+                    new Learner{FirstMidName="Meredith", LastName="Alonso", EnrollmentDate=DateTime.Parse("2002-09-01"), MajorID=2}
+                };
+                context.Learners.AddRange(learners);
+                context.SaveChanges();
+
+                // Thêm Course
+                var courses = new Course[]
+                {
+                    new Course{CourseID=1050, Title="Chemistry", Credits=3},
+                    new Course{CourseID=4022, Title="Microeconomics", Credits=3},
+                    new Course{CourseID=4041, Title="Macroeconomics", Credits=3}
+                };
+                context.Courses.AddRange(courses);
+                context.SaveChanges();
+
+                // Thêm Enrollment
+                var enrollments = new Enrollment[]
+                {
+                    new Enrollment{LearnerID=1, CourseID=1050, Grade=5.5f},
+                    new Enrollment{LearnerID=1, CourseID=4022, Grade=7.5f},
+                    new Enrollment{LearnerID=2, CourseID=1050, Grade=3.5f},
+                    new Enrollment{LearnerID=2, CourseID=4041, Grade=7f}
+                };
+                context.Enrollments.AddRange(enrollments);
+                context.SaveChanges();
+            }
+        }
+    }
+}
